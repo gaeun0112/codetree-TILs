a46714dce3aceeb74b0fac7215fb3f8793ebd08d
Gaeun Seo <85860941+gaeun0112@users.noreply.github.com>
2024-05-14 21:15:24 +0900

    Title: 전화번호 바꾸기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240514/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int a = sc.nextInt(), x = sc.nextInt(), y = sc.nextInt();
+        System.out.printf("010-%d-%d",y,x);
+    }
+}
